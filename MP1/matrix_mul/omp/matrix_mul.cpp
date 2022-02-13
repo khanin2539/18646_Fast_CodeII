2f613a30ebba75d0a0fbc25cb3241cc02996798f
Mitchell F. Yang <mfy@andrew.cmu.edu>
2022-02-12 21:41:33 -0500

    2.12x speedup

--- MP1/matrix_mul/omp/matrix_mul.cpp
+++ MP1/matrix_mul/omp/matrix_mul.cpp
@@ -18,22 +18,44 @@
 
 #include <omp.h>
 #include "matrix_mul.h"
+#include <string.h>
+#define IMPROVED
 
-namespace omp
-{
-  void
-  matrix_multiplication(float *sq_matrix_1, float *sq_matrix_2, float *sq_matrix_result, unsigned int sq_dimension )
-  {
-#pragma omp parallel for
-    for (unsigned int i = 0; i < sq_dimension; i++) 
-      {
-	for(unsigned int j = 0; j < sq_dimension; j++) 
-	  {       
-	    sq_matrix_result[i*sq_dimension + j] = 0;
-	    for (unsigned int k = 0; k < sq_dimension; k++)
-	      sq_matrix_result[i*sq_dimension + j] += sq_matrix_1[i*sq_dimension + k] * sq_matrix_2[k*sq_dimension + j];
-	  }
-      }// End of parallel region
+namespace omp{
+    void  matrix_multiplication(float *sq_matrix_1, float *sq_matrix_2, float *sq_matrix_result, unsigned int sq_dimension ){
+
+    #ifndef IMPROVED
+        #pragma omp parallel for 
+        for (unsigned int i = 0; i < sq_dimension; i++){
+        
+            for(unsigned int j = 0; j < sq_dimension; j++){       
+                sq_matrix_result[i*sq_dimension + j] = 0;
+                    for (unsigned int k = 0; k < sq_dimension; k++) {
+                    
+                    sq_matrix_result[i*sq_dimension + j] += sq_matrix_1[i*sq_dimension + k] * sq_matrix_2[k*sq_dimension + j];
+                }
+            }
+        }// End of parallel region
+    #else
+        memset(sq_matrix_result,0,sq_dimension*sq_dimension*sizeof(float));
+        #pragma omp parallel 
+            { 
+                int id = omp_get_thread_num();
+                int threads = omp_get_num_threads();
+                for (unsigned int k = 0; k < sq_dimension; k++) {
+                    int offset = k*sq_dimension;
+                    for (unsigned int i = id; i < sq_dimension; i+=threads){
+                        int ind = i*sq_dimension;
+                        #pragma unroll(4)
+                        for(unsigned int j = 0; j < sq_dimension; j++){       
+                            
+                            
+                            sq_matrix_result[ind + j] += sq_matrix_1[ind + k] * sq_matrix_2[offset + j];
+                        }
+                    }
+                }
+            }// End of parallel region
+    #endif
   }
   
 } //namespace omp
