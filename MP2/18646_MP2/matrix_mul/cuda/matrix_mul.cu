59ced5bfdabb0418b0c9c411e64d4a052086fb16
nurolinker <45439850+nurolinker@users.noreply.github.com>
2022-03-25 12:19:03 -0400

    198 gflops

--- MP2/18646_MP2/matrix_mul/cuda/matrix_mul.cu
+++ MP2/18646_MP2/matrix_mul/cuda/matrix_mul.cu
@@ -3,12 +3,10 @@
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -17,41 +15,63 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include "matrix_mul.h"
-#define BLOCK_WIDTH 2
+#define BLOCK_WIDTH 32
 #define BLOCK_X 32
 #define BLOCK_Y 32
-// #define OPTIMIZED 1
+#define OPTIMIZED 1
 #ifdef OPTIMIZED
 namespace cuda
 {
-    __global__ void matrix_mul_kernel(float *sq_matrix_1, float *sq_matrix_2, float *sq_matrix_result, int sq_dimension){
-        int col = blockIdx.x * blockDim.x + threadIdx.x;
-        int row = blockIdx.y * blockDim.y + threadIdx.y;
-        const unsigned int bx = BLOCK_X, by = BLOCK_Y;
-        const unsigned int tx = threadIdx.x, ty = threadIdx.y;
-        const unsigned int I = blockIdx.x*bx + tx, J = blockIdx.y*by + ty;
-        const unsigned int gx = gridDim.x, gy = gridDim.y;
-        __shared__ float a[BLOCK_X][BLOCK_Y], b[BLOCK_X][BLOCK_Y]; 
 
-        for(int k = 0; k < sq_dimension; k++){	
-            sq_matrix_result[row*sq_dimension + col] += sq_matrix_1[row * sq_dimension + k] * sq_matrix_2[k * sq_dimension + col];
-        }
-        //reduce
-        unsigned int t = threeadIdx.x;
-        unsigned int N = sq_dimension;
+    static inline int nextPowerOfTwo(int n) {
+      n--;
+
+      n = n >>  1 | n;
+      n = n >>  2 | n;
+      n = n >>  4 | n;
+      n = n >>  8 | n;
+      n = n >> 16 | n;
+      //n = n >> 32 | n;    //  For 64-bit ints
+
+      return ++n;
+    }
+    __global__ void matrix_mul_kernel(float *sq_matrix_1, float *sq_matrix_2, float *sq_matrix_result, int sq_dimension, int out_dimension){
         int col = blockIdx.x * blockDim.x + threadIdx.x;
         int row = blockIdx.y * blockDim.y + threadIdx.y;
-        for (unsigned int stride = 1; stride < blockDim.x; stride <<= 1){
+        int row_sq = row * sq_dimension;
+        if(row < sq_dimension && col < sq_dimension) {
+          float result = 0.0;
+          float matrix_1_data = 0.0;
+          float matrix_2_data = 0.0;
+          
+          extern __shared__ unsigned int sq_1[32 * 32];
+          extern __shared__ unsigned int sq_2[32 * 32];
+          
+          unsigned int * sq_1_idx = (unsigned int *) sq_1;
+          unsigned int * sq_2_idx = (unsigned int *) sq_2;
+          
+
+          
+            
             __syncthreads();
-            if ((t < stride) && (N < col) && (N < row)){
-                //whatever reduction
-                ehh[t] += ehh[t+stride];
+            
+            for(int k = 0; k < sq_dimension; k++){	
+                //result += sq_1[row * sq_dimension + k] * sq_2[k * sq_dimension + col];
+                //sq_matrix_result[row*sq_dimension + col] += sq_1[row * sq_dimension + k] * sq_2[k * sq_dimension + col];
+              matrix_1_data = sq_matrix_1[row * sq_dimension + k];
+              matrix_2_data = sq_matrix_2[k * sq_dimension + col];
+              /*for(int i = 0; i < matrix_2_data; i++) {
+                result += matrix_1_data;
+              }*/
+              result += matrix_1_data * matrix_2_data;
             }
+            sq_matrix_result[row*sq_dimension + col] = result;
         }
     }
 
     void matrix_multiplication(float *sq_matrix_1, float *sq_matrix_2, float *sq_matrix_result, unsigned int sq_dimension){
-        int size = sq_dimension * sq_dimension * sizeof(float);
+        int out_size =  (sq_dimension) * (sq_dimension) * sizeof(float);
+        int size = (sq_dimension) * (sq_dimension) * sizeof(float);
         float *sq_matrix_1_d, *sq_matrix_2_d, *sq_matrix_result_d;
 
         /***************************************************
@@ -60,13 +80,15 @@ namespace cuda
 
         /* copy sq_matrix_1 and sq_matrix_2 to device memory */
         cudaMalloc((void**) &sq_matrix_1_d, size);
+        cudaMemset(sq_matrix_1_d, 0, size);
         cudaMemcpy(sq_matrix_1_d, sq_matrix_1, size, cudaMemcpyHostToDevice);
         cudaMalloc((void**) &sq_matrix_2_d, size);
+        cudaMemset(sq_matrix_2_d, 0, size);
         cudaMemcpy(sq_matrix_2_d, sq_matrix_2, size, cudaMemcpyHostToDevice);
 
         /*allocate sq_matrix_result on host */
         cudaMalloc((void**) &sq_matrix_result_d, size);
-
+        cudaMemset(sq_matrix_result_d, 0, size);
         /***************************************************
         Step 2: Invoke kernel 
         ****************************************************/
@@ -74,7 +96,7 @@ namespace cuda
         dim3 dimBlock(BLOCK_WIDTH, BLOCK_WIDTH);
         dim3 dimGrid(blockNum, blockNum);
 
-        matrix_mul_kernel<<<dimGrid, dimBlock>>>(sq_matrix_1_d, sq_matrix_2_d, sq_matrix_result_d, sq_dimension);
+        matrix_mul_kernel<<<dimGrid, dimBlock>>>(sq_matrix_1_d, sq_matrix_2_d, sq_matrix_result_d, (sq_dimension), sq_dimension);
 
         /***************************************************
         Step 3: Transfer result from device to host 
@@ -89,17 +111,35 @@ namespace cuda
 #else
 namespace cuda
 {
-    __global__ void matrix_mul_kernel(float *sq_matrix_1, float *sq_matrix_2, float *sq_matrix_result, int sq_dimension){
+
+    static inline int nextPowerOfTwo(int n) {
+      n--;
+
+      n = n >>  1 | n;
+      n = n >>  2 | n;
+      n = n >>  4 | n;
+      n = n >>  8 | n;
+      n = n >> 16 | n;
+      //n = n >> 32 | n;    //  For 64-bit ints
+
+      return ++n;
+    }
+    __global__ void matrix_mul_kernel(float *sq_matrix_1, float *sq_matrix_2, float *sq_matrix_result, int sq_dimension, int out_dimension){
         int col = blockIdx.x * blockDim.x + threadIdx.x;
         int row = blockIdx.y * blockDim.y + threadIdx.y;
 
-        for(int k = 0; k < sq_dimension; k++){	
-            sq_matrix_result[row*sq_dimension + col] += sq_matrix_1[row * sq_dimension + k] * sq_matrix_2[k * sq_dimension + col];
+
+
+        if(row < sq_dimension && col < sq_dimension) {
+          for(int k = 0; k < sq_dimension; k++){	
+              sq_matrix_result[row*sq_dimension + col] += sq_matrix_1[row * sq_dimension + k] * sq_matrix_2[k * sq_dimension + col];
+          }
         }
     }
 
     void matrix_multiplication(float *sq_matrix_1, float *sq_matrix_2, float *sq_matrix_result, unsigned int sq_dimension){
-        int size = sq_dimension * sq_dimension * sizeof(float);
+        int out_size =  (sq_dimension) * (sq_dimension) * sizeof(float);
+        int size = (sq_dimension) * (sq_dimension) * sizeof(float);
         float *sq_matrix_1_d, *sq_matrix_2_d, *sq_matrix_result_d;
 
         /***************************************************
@@ -108,13 +148,15 @@ namespace cuda
 
         /* copy sq_matrix_1 and sq_matrix_2 to device memory */
         cudaMalloc((void**) &sq_matrix_1_d, size);
+        cudaMemset(sq_matrix_1_d, 0, size);
         cudaMemcpy(sq_matrix_1_d, sq_matrix_1, size, cudaMemcpyHostToDevice);
         cudaMalloc((void**) &sq_matrix_2_d, size);
+        cudaMemset(sq_matrix_2_d, 0, size);
         cudaMemcpy(sq_matrix_2_d, sq_matrix_2, size, cudaMemcpyHostToDevice);
 
         /*allocate sq_matrix_result on host */
         cudaMalloc((void**) &sq_matrix_result_d, size);
-
+        cudaMemset(sq_matrix_result_d, 0, size);
         /***************************************************
         Step 2: Invoke kernel 
         ****************************************************/
@@ -122,7 +164,7 @@ namespace cuda
         dim3 dimBlock(BLOCK_WIDTH, BLOCK_WIDTH);
         dim3 dimGrid(blockNum, blockNum);
 
-        matrix_mul_kernel<<<dimGrid, dimBlock>>>(sq_matrix_1_d, sq_matrix_2_d, sq_matrix_result_d, sq_dimension);
+        matrix_mul_kernel<<<dimGrid, dimBlock>>>(sq_matrix_1_d, sq_matrix_2_d, sq_matrix_result_d, (sq_dimension), sq_dimension);
 
         /***************************************************
         Step 3: Transfer result from device to host 
